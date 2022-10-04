6ff7cf191442e15cdf335279217816d3a87defa0
Sandra3217 <christophersandra46@gmail.com>
2022-10-03 23:14:08 -0700

    added betty coding style

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
 * _print_rev_recursion - prints string in reverse
-* @char *s: points us to the address of the first memory
+* @*s: points us to the address of the first memory
 * Return: void
 */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0') 
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		putchar(*s);
