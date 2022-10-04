c553dc5b40e90ca172e9b292ec8f98ec3a789060
Sandra3217 <christophersandra46@gmail.com>
2022-10-03 22:52:55 -0700

    added betty coding style

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-* _print_rev_recursion: function name
-* *s: pointer address
+* _print_rev_recursion - address of function
+* *s: points us to the address of the first memory
+* s: initial memory location
 */
 void _print_rev_recursion(char *s)
 {
-
 	if (*s != '\0') 
 	{
-       	   	_print_rev_recursion(s + 1);
-        	putchar(*s);
-	} 
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
 }
