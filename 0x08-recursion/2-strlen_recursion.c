b56dc94e1a09da26d7ff2670be43a6ef844e032a
Sandra3217 <christophersandra46@gmail.com>
2022-10-04 00:07:35 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,13 @@
 #include "main.h"
+/**
+* _strlen_recursion - returns the length of a string
+* @s: points to the memory address
+* Return: integer
+*/
 int _strlen_recursion(char *s)
 {
+	if (*s == '\0') {
+	return 0;
+	}
+	return 1 +_strlen_recursion(s + 1);
 }
