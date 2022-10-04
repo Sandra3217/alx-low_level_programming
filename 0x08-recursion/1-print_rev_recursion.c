bcb8e7a9a666663e4db96d615461efa12f97720b
Sandra3217 <christophersandra46@gmail.com>
2022-10-03 22:37:55 -0700

    added betty coding style

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
+
 /**
- * _print_rev_recursion: function name
- * *s: pointer address
+* _print_rev_recursion: function name
+* *s: pointer address
 */
 void _print_rev_recursion(char *s)
 {
-       	if (*s != '\0') 
+
+	if (*s != '\0') 
 	{
-
        	   	_print_rev_recursion(s + 1);
-
-        	putchar(*s);
-        
+        	putchar(*s)
 	} 
-
 }
